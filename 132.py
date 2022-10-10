c3ca1519c67ab729e8c8ac04d347e26e37059e8a
Vladimir <vakharitonov2004@gmail.com>
2022-10-10 21:24:21 +0300

    Дружелюбный интерфейс

--- 132.py
+++ 132.py
@@ -1,5 +1,8 @@
 from math import *
+print("Здравствуйте! Это программа для нахождения триогонометрических функций")
+print("Введите число градусов:")
 n = radians(int(input()))
+print("Ответ:")
 print('sin = ', sin(n))
 print('cos = ', cos(n))
 print('tan = ', tan(n))
